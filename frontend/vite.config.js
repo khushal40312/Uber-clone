2a86dcb4fcb008a6e8a84d5c6f9a27da75c26a44
khushal <khushalsharma40312@gmail.com>
2025-04-25 10:26:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,25 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(),tailwindcss()],
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Group all react-related stuff together
-            if (id.match(/react|react-dom|react-router|scheduler/)) {
-              return 'react-vendor'
-            }
-            // Separate heavy libraries like leaflet
-            if (id.includes('leaflet')) return 'leaflet-vendor'
-            if (id.includes('react-toastify')) return 'toastify-vendor'
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
+  plugins: [react(),tailwindcss()],
 })
